1271fe315aacb5e00315e7d2ff2bb89794d100c6
Jerid Francom <francojc@wfu.edu>
2018-12-03 12:56:48 -0500

    Wrangle data

---
+++ R/sdac_speaker_meta.R
@@ -0,0 +1,24 @@
+#' Switchboard Dialog Act Corpus Metadata
+#'
+#' A dataset of the corpus files containing metata for 440 speakers of American
+#' English.
+#'
+#' @format A data frame with 543 rows and 14 variables:
+#' \describe{
+#'   \item{speaker_id}{Unique speaker identification code}
+#'   \item{pin}{Access code}
+#'   \item{target}{...}
+#'   \item{sex}{Sex of the speaker}
+#'   \item{birth_year}{Year that the speaker was born}
+#'   \item{dialect_area}{Region from the US where the speaker spent first 10 years}
+#'   \item{education}{Highest educational level attained}
+#'   \item{ti}{...}
+#'   \item{payment_type}{Form of payment for participation}
+#'   \item{amt_pd}{Payment amount for participation}
+#'   \item{con}{...}
+#'   \item{remarks}{Misc. comments}
+#'   \item{calls_deleted}{...}
+#'   \item{speaker_partition}{...}
+#' }
+#' @source \url{https://catalog.ldc.upenn.edu/docs/LDC97S62/}
+"sdac_speaker_meta"
