2a20199c9d15fd224ddfb5dae44cfdae3d267345
Jerid Francom <francojc@wfu.edu>
2019-02-25 10:32:08 -0500

    Added exploration vignette and sms data and deleted the default functions hello() and printer().

---
+++ R/sms.R
@@ -0,0 +1,15 @@
+#' SMS Spam Collection v. 1
+#'
+#' The SMS Spam Collection v.1 is a public set of SMS labeled messages that
+#' have been collected for mobile phone spam research. It has one collection
+#' composed by 5,574 English, real and non-enconded messages, tagged according
+#' being legitimate (ham) or spam.
+#'
+#'
+#' @format A data frame with 4,837 rows and 2 variables:
+#' \describe{
+#'   \item{sms_type}{Type of message; either spam or ham}
+#'   \item{message}{The sms message}
+#' }
+#' @source \url{http://www.dt.fee.unicamp.br/~tiago/smsspamcollection/smsspamcollection.zip}
+"sms"
